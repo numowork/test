acf1a3ae5ac17bea00c1d6b76826c4ed5d7e9640
numowork <numowork@gmail.com>
2022-04-21 16:44:18 +0300

    test

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "dsf"
 #mongo test1
-mongo test --eval "db.restaurants.find()"
-mongo test --eval "db.restaurants.find({borough:'Queens'})"
+mongo test --eval "db.restaurants.find({borough:'d'})"
+mongo test --eval "db.restaurants.find({borough:'Queens'}).pretty()"
 #mongo test --eval "printjson(db.getCollectionNames())"
 #eval "db.hello()"
