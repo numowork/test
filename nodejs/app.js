cf847d290640468bf0aa00de30e28938592b74d1
numowork <numowork@gmail.com>
2022-04-22 15:29:49 +0300

    test db

--- nodejs/app.js
+++ nodejs/app.js
@@ -79,7 +79,7 @@ async function start(){
     const url =  `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
     console.log(url)
     const dd = await mongoose.connect(url, {useNewUrlParser: true});
-    console.log(dd.db)
+    console.log(dd)
     // console.log(dd.db.collections())
     // console.log(dd.client)
 }
