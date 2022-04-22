e8e9b5d6ca014d1911c673f5722fa5bec0bf9b5f
numowork <numowork@gmail.com>
2022-04-22 14:23:43 +0300

    test db

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 // let mongoose = require('mongoose');
 //
@@ -13,7 +14,7 @@ app.use(express.json())
 // console.log("Database connected successfully");
 // console.log(db.logs.find())
 // });
-const {MongoClient} = require('mongodb')
+// const {MongoClient} = require('mongodb')
 //
 // let x = new Mongo('mongodb://127.0.0.1:27017/');
 // let mydb = x.getDB('apple');
@@ -51,17 +52,32 @@ const {MongoClient} = require('mongodb')
 // }
 
 // let MongoClient = require('mongodb').MongoClient
-    , format = require('util').format;
-MongoClient.connect('mongodb://127.0.0.1:27017/', function (err, db) {
-    if (err) throw err;
-    console.log(db)
-    console.log(JSON.parse(db))
-    // let collection = db.collection('logs');
-    // collection.insert({a: 2}, function (err, docs) {
-    //     collection.count(function (err, count) {
-    //         console.log(format("count = %s", count));
-    //         db.close();
-    //     });
-    // });
-});
+//     , format = require('util').format;
+// MongoClient.connect('mongodb://127.0.0.1:27017/', function (err, db) {
+//     if (err) throw err;
+//     console.log(db)
+//     console.log(JSON.parse(db))
+//     // let collection = db.collection('logs');
+//     // collection.insert({a: 2}, function (err, docs) {
+//     //     collection.count(function (err, count) {
+//     //         console.log(format("count = %s", count));
+//     //         db.close();
+//     //     });
+//     // });
+// });
+
+const mongoose = require('mongoose');
+
+const MONGO_USERNAME = 'numowork';
+const MONGO_PASSWORD = '12345';
+const MONGO_HOSTNAME = '127.0.0.1';
+const MONGO_PORT = '27017';
+const MONGO_DB = 'apple';
+
+async function start(){
+    const url = await `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+    console.log(url)
+    const dd = await mongoose.connect(url, {useNewUrlParser: true});
+    console.log(dd)
+}
 
