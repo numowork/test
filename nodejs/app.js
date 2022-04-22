ff7045fcf691f83d155b970681c003702cf5bf06
numowork <numowork@gmail.com>
2022-04-22 13:17:58 +0300

    test db

--- nodejs/app.js
+++ nodejs/app.js
@@ -5,5 +5,6 @@ let db = mongoose.connect('mongodb://127.0.0.1:27017/test');
 
 // testing connectivity
 mongoose.connection.once('connected', function() {
-console.log("Database connected successfully")
+console.log("Database connected successfully");
+console.log(db)
 });
