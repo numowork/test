a6bb1835069ecd3265a9bdb922a66485f6181676
numowork <numowork@gmail.com>
2022-04-22 13:20:38 +0300

    test db

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,10 +1,10 @@
 let mongoose = require('mongoose');
 
 // Connecting local mongodb database named test
-let db = mongoose.connect('mongodb://127.0.0.1:27017/test');
+let db = mongoose.connect('mongodb://127.0.0.1:27017/apple');
 
 // testing connectivity
 mongoose.connection.once('connected', function() {
 console.log("Database connected successfully");
-console.log(db.getCollectionNames())
+console.log(db.logs.find())
 });
