10a2f86dce2aba2c1e20f960bbabe27a1f4a3468
numowork <numowork@gmail.com>
2022-04-22 13:38:06 +0300

    test db

--- nodejs/app.js
+++ nodejs/app.js
@@ -8,9 +8,39 @@
 // console.log("Database connected successfully");
 // console.log(db.logs.find())
 // });
-let {Mongo}= require('mongodb')
-
-let x = new Mongo('mongodb://127.0.0.1:27017/');
-let mydb = x.getDB('apple');
-console.log(mydb)
+const {MongoClient} = require('mongodb')
+//
+// let x = new Mongo('mongodb://127.0.0.1:27017/');
+// let mydb = x.getDB('apple');
+// console.log(mydb)
 // var mydb = connect('host[:port]/mydb');
+
+async function main() {
+    const url = 'mongodb://127.0.0.1:27017/'
+
+    const client = new MongoClient(url)
+
+    try {
+        console.log("working")
+        await client.connect()
+
+    } catch (e) {
+        console.log(e)
+    } finally {
+        await client.close()
+    }
+
+
+}
+
+main().catch(console.error)
+
+async function listDatabases(client) {
+    const databasesClient = await client.db.admin().listDatabases();
+
+    console.log("Databases:")
+    databasesClient.databases.forEach(db => {
+        console.log(`--${db.name}`)
+    })
+}
+
