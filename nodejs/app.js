3d1891e0fe9ef9fa6acb7857ce4cfdc69bec8d8f
numowork <numowork@gmail.com>
2022-04-22 13:29:25 +0300

    test db

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,10 +1,16 @@
-let mongoose = require('mongoose');
+// let mongoose = require('mongoose');
+//
+// // Connecting local mongodb database named test
+// let db = mongoose.connect('mongodb://127.0.0.1:27017/apple');
+//
+// // testing connectivity
+// mongoose.connection.once('connected', function() {
+// console.log("Database connected successfully");
+// console.log(db.logs.find())
+// });
+let {Mongo}= require('mongodb')
 
-// Connecting local mongodb database named test
-let db = mongoose.connect('mongodb://127.0.0.1:27017/apple');
-
-// testing connectivity
-mongoose.connection.once('connected', function() {
-console.log("Database connected successfully");
-console.log(db.logs.find())
-});
+let x = new Mongo('mongodb://127.0.0.1:27017/');
+let mydb = x.getDB('apple');
+console.log(mydb)
+// var mydb = connect('host[:port]/mydb');
