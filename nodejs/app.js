e80883bac10ecd1654709a3432fc19ff11de106b
numowork <numowork@gmail.com>
2022-04-22 13:55:30 +0300

    test db

--- nodejs/app.js
+++ nodejs/app.js
@@ -9,40 +9,50 @@
 // console.log(db.logs.find())
 // });
 const {MongoClient} = require('mongodb')
-const Console = require("console");
 //
 // let x = new Mongo('mongodb://127.0.0.1:27017/');
 // let mydb = x.getDB('apple');
 // console.log(mydb)
 // var mydb = connect('host[:port]/mydb');
 
-async function main() {
-    const url = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.3.1'
+// async function main() {
+//     const url = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.3.1'
+//
+//     const client = new MongoClient(url)
+//
+//     try {
+//         console.log("working")
+//         await client.connect()
+//         // client.db
+//         await listDatabases(client)
+//     } catch (e) {
+//         console.log(e)
+//     } finally {
+//         await client.close()
+//     }
+//
+//
+// }
+//
+// main().catch(console.error)
+//
+// async function listDatabases(client) {
+//     const databasesClient = await client.db('apple').collectionNames();
+//
+//     // console.log("Databases:", databasesClient)
+//     databasesClient.databases.forEach(db => {
+//         console.log(`--${db.name}`)
+//     })
+// }
 
-    const client = new MongoClient(url)
-
-    try {
-        console.log("working")
-        await client.connect()
-        // client.db
-        await listDatabases(client)
-    } catch (e) {
-        console.log(e)
-    } finally {
-        await client.close()
+let MongoClient = require('mongodb').MongoClient
+    , format = require('util').format;
+MongoClient.connect('mongodb://127.0.0.1:27017/test', function (err, db) {
+    if (err) {
+        throw err;
+    } else {
+        console.log("successfully connected to the database");
     }
-
-
-}
-
-main().catch(console.error)
-
-async function listDatabases(client) {
-    const databasesClient = await client.db('apple').collectionNames();
-
-    // console.log("Databases:", databasesClient)
-    databasesClient.databases.forEach(db => {
-        console.log(`--${db.name}`)
-    })
-}
+    db.close();
+});
 
