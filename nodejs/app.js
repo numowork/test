03ff346a32f7b4572e91cd1087afc343ae3cb092
numowork <numowork@gmail.com>
2022-04-22 13:57:20 +0300

    test db

--- nodejs/app.js
+++ nodejs/app.js
@@ -47,12 +47,16 @@ const {MongoClient} = require('mongodb')
 
 // let MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
-MongoClient.connect('mongodb://127.0.0.1:27017/test', function (err, db) {
-    if (err) {
-        throw err;
-    } else {
-        console.log("successfully connected to the database");
-    }
-    db.close();
+MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+    if(err) throw err;
+
+    let collection = db.collection('test_insert');
+    collection.insert({a:2}, function(err, docs) {
+        collection.count(function(err, count) {
+            console.log(format("count = %s", count));
+            db.close();
+        });
+    });
 });
+
 
