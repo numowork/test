f17b89b3ce831cfc15f4186456b9824d0c183eb6
numowork <numowork@gmail.com>
2022-04-22 14:26:58 +0300

    test db

--- nodejs/app.js
+++ nodejs/app.js
@@ -72,7 +72,7 @@ const MONGO_USERNAME = 'numowork';
 const MONGO_PASSWORD = '12345';
 const MONGO_HOSTNAME = '127.0.0.1';
 const MONGO_PORT = '27017';
-// const MONGO_DB = 'apple';
+const MONGO_DB = 'apple';
 
 async function start(){
     const url = await `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
