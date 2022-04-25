fd95397aef26caeb4c380b1418716c972a04f594
numowork <numowork@gmail.com>
2022-04-25 14:59:03 +0300

    rm app.log

--- dirnameArray.sh
+++ dirnameArray.sh
@@ -8,6 +8,8 @@ for key in "${brands[@]}"; do
   echo "$key"
 #  cp /srv/theplatform/app.log  /srv/logs/$key/app.log
   mongoimport --db $key --collection logs --file /srv/logs/$key/app.log
+  echo 'importing process'
+  rm /srv/logs/$key/app.log
 done
 
 echo "$"
